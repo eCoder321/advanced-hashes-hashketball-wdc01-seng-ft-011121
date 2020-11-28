32b33006eec10342d6346a79e843c453b520e2d0
Fem Adisa <femiwonderful@gmail.com>
2020-11-28 19:11:32 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,5 @@
 # Write your code below game_hash
+require 'pry'
 def game_hash
   {
     home: {
@@ -127,3 +128,141 @@ def game_hash
 end
 
 # Write code here
+
+
+
+
+
+
+
+
+def winning_team
+  winning_score = 0 
+  winner = ""
+    game_hash.map {
+    |location, team_info, count = 0|
+      game_hash[location][:players].map {
+        |each_player|
+        count += each_player[:points]
+      }
+      if count > winning_score
+        winning_score = count 
+        winner = "#{game_hash[location][:team_name]} is the winner!"
+      end
+    }
+    winner
+end
+#p winning_team
+"""PLS COME BACKand figure out how to use player_numbers and num_points_scored to solve this problem"""
+
+
+def player_with_longest_name(least_biggest_type = 0, conditional_stat = :player_name)
+  winning_player = ""
+  biggest_number = least_biggest_type
+  game_hash.map {
+    |location, team_info|
+      game_hash[location][:players].map {
+        |each_player|
+        if each_player[conditional_stat].length > biggest_number
+          biggest_number = each_player[conditional_stat].length
+          winning_player = each_player[:player_name]
+        end
+        #binding.pry
+      }
+  }
+  winning_player
+end
+#p player_with_longest_name
+
+
+def player_with_most_x(least_biggest_type = 0, conditional_stat)
+  winning_player = ""
+  biggest_number = least_biggest_type
+  game_hash.map {
+    |location, team_info|
+      game_hash[location][:players].map {
+        |each_player|
+        if each_player[conditional_stat] > biggest_number
+          biggest_number = each_player[conditional_stat] 
+          winning_player = each_player[:player_name]
+        end
+        #binding.pry
+      }
+  }
+  winning_player
+end
+#p player_with_most_x(:shoe)
+
+
+def most_points_scored
+  player_with_most_x(:points)
+end
+#p most_points_scored
+
+def big_shoe_rebounds
+  player_stats(player_with_most_x(:shoe))[:rebounds]
+  #binding.pry
+end
+
+def player_stats(player)
+  player_here(player)
+end
+
+def player_numbers(team)
+  game_hash.collect {
+    |location, team_info|
+    if team_info.values.include? team
+      game_hash[location][:players].collect {
+        |i|
+        i[:number]
+      }
+    end
+  }.flatten.compact
+end
+
+
+def player_here(player)
+  game_hash.select {
+    |i, a|
+    game_hash[i][:players].select {
+      |b|
+      points = b.select {
+        |c, d|
+        return b if d == player
+      }
+      #binding.pry
+    }
+  }
+end
+
+def shoe_size(player)
+  player_here(player)[:shoe]
+end
+
+def team_names
+  game_hash.collect {
+    |i,a|
+    game_hash[i][:team_name]
+  }
+end
+
+def team_colors(team)
+  game_hash.collect {
+    |a, i|
+    return game_hash[a][:colors] if i[:team_name] == team
+  }
+end
+
+def num_points_scored(player)
+ player_here(player)[:points]
+end
+
+def long_name_steals_a_ton?
+  candidate = player_with_longest_name
+  condition = player_with_most_x(:steals)
+  "#{true}, #{condition} had the most steals :)" if candidate == condition
+end
+p long_name_steals_a_ton?
+
+
+p "HALLELUJAYH!"
